6817faee15ca1e2eacc14e7b7df8c2373ccaabda
cthomas0087 <61051296+cthomas0087@users.noreply.github.com>
2020-02-18 09:49:14 +0000

    Update IF Statements: Number less than.py

--- IF Statements: Number less than.py
+++ IF Statements: Number less than.py
@@ -1,6 +1,5 @@
 ## write a program that prints out all the elements of the list that are less than 5.
-a = [7, 5, 10, 2, 4, 6, 12, 17, 1, 1, 8, 3]
-
-for i in a:
+n = [7, 5, 10, 2, 4, 6, 12, 17, 1, 1, 8, 3]
+for i in n:
     if i<5:
         print(i)
